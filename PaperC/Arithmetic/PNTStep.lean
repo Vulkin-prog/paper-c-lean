ed7ac604974d3f7b5c2e25f3bb6e37fd9c4624a7
Vulkin-prog <brice.pouly@gmail.com>
2026-08-13 15:01:47 +0200

    formalization: close the natural-input PNT equivalence

--- PaperC/Arithmetic/PNTStep.lean
+++ PaperC/Arithmetic/PNTStep.lean
@@ -5,4 +5,12 @@ namespace PaperC.PrimeNumberTheoremInput
 open Filter Asymptotics
 open scoped Topology
 
+theorem primeCounting_isEquivalent_nat_closed :
+    IsEquivalent atTop
+      (fun L : Nat => (Nat.primeCounting L : Real))
+      (fun L : Nat => (L : Real) / Real.log (L : Real)) :=
+  (composed_pnt_equivalence.congr_left
+    (Filter.Eventually.of_forall composed_primeCounting_apply)).congr_right
+      (Filter.Eventually.of_forall composed_scale_apply)
+
 end PaperC.PrimeNumberTheoremInput
