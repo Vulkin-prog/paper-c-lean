06098267e3597d17c4f4846bbfe15d8cd5b335ab
Vulkin-prog <brice.pouly@gmail.com>
2026-08-13 15:06:14 +0200

    formalization: derive the standard-form PNT limit

--- PaperC/Arithmetic/PNTStandardClosed2.lean
+++ PaperC/Arithmetic/PNTStandardClosed2.lean
@@ -5,4 +5,13 @@ namespace PaperC.PrimeNumberTheoremInput
 open Filter
 open scoped Topology
 
+theorem primeCounting_standard_tendsto_closed :
+    Tendsto
+      (fun L : Nat =>
+        (Nat.primeCounting L : Real) *
+          Real.log (L : Real) / (L : Real))
+      atTop (nhds 1) :=
+  primeCounting_ratio_tendsto_closed.congr'
+    (Filter.Eventually.of_forall ratio_eq_standard_closed)
+
 end PaperC.PrimeNumberTheoremInput
