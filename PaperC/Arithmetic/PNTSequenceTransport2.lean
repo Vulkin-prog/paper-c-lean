4aefcf76bbf89b43317ef0001a4b4304906b0ba5
Vulkin-prog <brice.pouly@gmail.com>
2026-08-13 14:58:06 +0200

    formalization: start the sequential PNT transport module

---
+++ PaperC/Arithmetic/PNTSequenceTransport2.lean
@@ -0,0 +1,9 @@
+import PaperC.Arithmetic.PrimeCountIdentityPNT
+import PrimeNumberTheoremAnd.Consequences
+
+namespace PaperC.PrimeNumberTheoremInput
+
+open Filter Asymptotics
+open scoped Topology
+
+end PaperC.PrimeNumberTheoremInput
